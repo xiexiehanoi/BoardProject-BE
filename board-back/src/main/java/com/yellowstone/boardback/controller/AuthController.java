6a2a1effd871945444b6a898be260d1a9786e9c2
samkim <ksw593vn@gmail.com>
2024-06-14 00:00:12 +0900

    Feat: AuthController 추가

---
+++ board-back/src/main/java/com/yellowstone/boardback/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.yellowstone.boardback.controller;
+
+import com.yellowstone.boardback.dto.request.auth.SignUpRequestDto;
+import com.yellowstone.boardback.dto.response.auth.SignUpResponseDto;
+import com.yellowstone.boardback.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<? super SignUpResponseDto> signUp(
+            @RequestBody @Valid SignUpRequestDto requestBody
+    ){
+      ResponseEntity<? super SignUpResponseDto> response = authService.signUp(requestBody);
+      return response;
+    }
+
+}
