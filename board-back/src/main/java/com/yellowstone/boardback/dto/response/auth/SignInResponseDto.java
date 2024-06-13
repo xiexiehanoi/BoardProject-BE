ec65421c8211b072e45292469530ee9b8f67cd94
samkim <ksw593vn@gmail.com>
2024-06-14 01:11:26 +0900

    Feat: SignIn requestDto-responseDto 추가

---
+++ board-back/src/main/java/com/yellowstone/boardback/dto/response/auth/SignInResponseDto.java
@@ -0,0 +1,33 @@
+package com.yellowstone.boardback.dto.response.auth;
+
+import com.yellowstone.boardback.common.ResponseCode;
+import com.yellowstone.boardback.common.ResponseMessage;
+import com.yellowstone.boardback.dto.response.ResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class SignInResponseDto extends ResponseDto {
+
+    private String token;
+    private int expirationTime;
+
+    private SignInResponseDto (String code, String message, String token) {
+        super(code, message);
+        this.token = token;
+        this.expirationTime = 3600;
+    }
+
+    public static ResponseEntity<SignInResponseDto> success(String token) {
+        SignInResponseDto result = new SignInResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, token);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> signInFailed() {
+        ResponseDto result = new ResponseDto(ResponseCode.SIGN_IN_FAIL, ResponseMessage.SIGN_IN_FAIL);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+
+}
