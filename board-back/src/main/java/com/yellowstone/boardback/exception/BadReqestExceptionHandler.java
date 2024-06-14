252e0f882eda5213bbcc3cf76d2585c69cc258fc
samkim <ksw593vn@gmail.com>
2024-06-14 11:43:12 +0900

    Feat: exceptionhanlder 추가

---
+++ board-back/src/main/java/com/yellowstone/boardback/exception/BadReqestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.yellowstone.boardback.exception;
+
+import com.yellowstone.boardback.dto.response.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BadReqestExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException .class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception) {
+        return ResponseDto.validationFailed();
+    }
+
+}
