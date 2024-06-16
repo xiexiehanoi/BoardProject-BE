1864aacb51b2727fc0b8eba8970423215c137cae
samkim <ksw593vn@gmail.com>
2024-06-16 23:33:28 +0900

    Feat: commentListItem, GetCommentListResultSet 추가 (게시글 댓글 리스트 기능)

---
+++ board-back/src/main/java/com/yellowstone/boardback/repository/resultSet/GetCommentListResultSet.java
@@ -0,0 +1,10 @@
+package com.yellowstone.boardback.repository.resultSet;
+
+public interface GetCommentListResultSet {
+
+    String getNickname();
+    String getProfileImage();
+    String getWriteDatetime();
+    String getContent();
+
+}
