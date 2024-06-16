61a4bce06e47c916c3ddbea1f30ba93cfc801eb7
samkim <ksw593vn@gmail.com>
2024-06-17 00:08:04 +0900

    Feat: DeleteBoardResponseDto 추가 (게시물 삭제)

---
+++ board-back/src/main/java/com/yellowstone/boardback/dto/response/board/DeleteBoardResponseDto.java
@@ -0,0 +1,34 @@
+package com.yellowstone.boardback.dto.response.board;
+
+import com.yellowstone.boardback.common.ResponseCode;
+import com.yellowstone.boardback.common.ResponseMessage;
+import com.yellowstone.boardback.dto.response.ResponseDto;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class DeleteBoardResponseDto extends ResponseDto {
+
+    private DeleteBoardResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<DeleteBoardResponseDto> success(){
+        DeleteBoardResponseDto result = new DeleteBoardResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistBoard(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_BOARD, ResponseMessage.NOT_EXISTED_BOARD);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+    public static ResponseEntity<ResponseDto> noExistUser(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+    public static ResponseEntity<ResponseDto> noPermission(){
+        ResponseDto result = new ResponseDto(ResponseCode.NO_PERMISSION, ResponseMessage.NO_PERMISSION);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
+    }
+}
