23dd730270f1cfc1aa2ee5da966694a21c9d4e3f
samkim <ksw593vn@gmail.com>
2024-06-16 23:51:57 +0900

    Fix: GetCommentListResponseDto - notexistboard 내용 추가 (게시판 댓글 리스트 기능)

--- board-back/src/main/java/com/yellowstone/boardback/dto/response/board/GetCommentListResponseDto.java
+++ board-back/src/main/java/com/yellowstone/boardback/dto/response/board/GetCommentListResponseDto.java
@@ -16,7 +16,7 @@ public class GetCommentListResponseDto extends ResponseDto {
 
     private List<CommentListItem> commentList;
 
-    public GetCommentListResponseDto(List<GetCommentListResultSet resultSets) {
+    public GetCommentListResponseDto(List<GetCommentListResultSet> resultSets) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         this.commentList = CommentListItem.copyList(resultSets);
     }
@@ -26,5 +26,8 @@ public class GetCommentListResponseDto extends ResponseDto {
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-    public static ResponseEntity<ResponseDto> noExist
+    public static ResponseEntity<ResponseDto> noExistBoard(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_BOARD, ResponseMessage.NOT_EXISTED_BOARD);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
 }
