399c85bf7006ac19968146bee6d95aa22cccb4c0
samkim <ksw593vn@gmail.com>
2024-06-17 00:55:21 +0900

    Feat: increaseViewCount Service 추가  (viewcount 기능)

--- board-back/src/main/java/com/yellowstone/boardback/service/BoardService.java
+++ board-back/src/main/java/com/yellowstone/boardback/service/BoardService.java
@@ -13,6 +13,7 @@ public interface BoardService {
     ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email);
     ResponseEntity<? super PostCommentResponseDto> postComment(PostCommentRequestDto dto,Integer boardNumber, String email);
     ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email);
+    ResponseEntity<? super IncreaseViewCountResponseDto> increaseViewCount(Integer boardNumber);
     ResponseEntity<? super DeleteBoardResponseDto> deleteBoard(Integer boardNumber, String email);
 
 
