e70313bbb8113b1fdcacd9e1eb48d4d5dfe33cf2
samkim <ksw593vn@gmail.com>
2024-06-17 15:39:47 +0900

    Feat: GetUserBoardListResponseDto 추가 (유저게시물 불러오기)

---
+++ board-back/src/main/java/com/yellowstone/boardback/dto/response/board/GetUserBoardListResponseDto.java
@@ -0,0 +1,32 @@
+package com.yellowstone.boardback.dto.response.board;
+
+import com.yellowstone.boardback.common.ResponseCode;
+import com.yellowstone.boardback.common.ResponseMessage;
+import com.yellowstone.boardback.dto.object.BoardListItem;
+import com.yellowstone.boardback.dto.response.ResponseDto;
+import com.yellowstone.boardback.entity.BoardListViewEntity;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Getter
+public class GetUserBoardListResponseDto extends ResponseDto {
+
+    private List<BoardListItem> userBoardList;
+
+    private GetUserBoardListResponseDto(List<BoardListViewEntity> boardListViewEntities) {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.userBoardList = BoardListItem.getList(boardListViewEntities);
+    }
+        public static ResponseEntity<GetUserBoardListResponseDto> success(List<BoardListViewEntity> boardListViewEntities){
+        GetUserBoardListResponseDto result = new GetUserBoardListResponseDto(boardListViewEntities);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
