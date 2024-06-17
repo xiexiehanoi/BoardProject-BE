9a9967fa493b314c7b236f2d47c0d3db5a7bdcc6
samkim <ksw593vn@gmail.com>
2024-06-17 15:26:56 +0900

    Feat: GetUser Service 추가 (유저 불러오기)

--- board-back/src/main/java/com/yellowstone/boardback/service/UserService.java
+++ board-back/src/main/java/com/yellowstone/boardback/service/UserService.java
@@ -1,11 +1,12 @@
 package com.yellowstone.boardback.service;
 
 import com.yellowstone.boardback.dto.response.user.GetSignInUserResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.yellowstone.boardback.dto.response.user.GetUserResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
 
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
 
 }
--- board-back/src/main/java/com/yellowstone/boardback/service/implement/UserServiceImplement.java
+++ board-back/src/main/java/com/yellowstone/boardback/service/implement/UserServiceImplement.java
@@ -2,6 +2,7 @@ package com.yellowstone.boardback.service.implement;
 
 import com.yellowstone.boardback.dto.response.ResponseDto;
 import com.yellowstone.boardback.dto.response.user.GetSignInUserResponseDto;
+import com.yellowstone.boardback.dto.response.user.GetUserResponseDto;
 import com.yellowstone.boardback.entity.UserEntity;
 import com.yellowstone.boardback.repository.UserRepository;
 import com.yellowstone.boardback.service.UserService;
@@ -32,5 +33,23 @@ public class UserServiceImplement implements UserService {
         return GetSignInUserResponseDto.success(userEntity);
 
 
+    }
+
+    @Override
+    public ResponseEntity<? super GetUserResponseDto> getUser(String email) {
+
+        UserEntity userEntity = null;
+
+        try {
+
+            userEntity = userRepository.findByEmail(email);
+            if (userEntity == null) return GetUserResponseDto.noExistUser();
+
+        } catch(Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetUserResponseDto.success(userEntity);
     }
 }
