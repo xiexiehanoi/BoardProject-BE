0ad7b2adf3fffbcee536ea47965c7e3d206b6c9f
samkim <ksw593vn@gmail.com>
2024-06-17 16:23:27 +0900

    Feat: Patch Nickname, profileImage RequestDto-ResponseDto 추가 (닉네임 및 프로필 이미지 수정)

---
+++ board-back/src/main/java/com/yellowstone/boardback/dto/request/user/PatchProfileImageRequestDto.java
@@ -0,0 +1,14 @@
+package com.yellowstone.boardback.dto.request.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatchProfileImageRequestDto {
+
+    private String ProfileImage;
+
+}
