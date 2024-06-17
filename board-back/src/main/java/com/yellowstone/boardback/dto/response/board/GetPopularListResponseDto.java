f51131faf6afbc0c65c1fb747876f6fdbbf54893
samkim <ksw593vn@gmail.com>
2024-06-17 11:16:59 +0900

    Fix: GetPoplularResultSet명 변경으로 인한 class명 변경

--- board-back/src/main/java/com/yellowstone/boardback/dto/response/board/GetPopularListResponseDto.java
+++ board-back/src/main/java/com/yellowstone/boardback/dto/response/board/GetPopularListResponseDto.java
@@ -3,7 +3,7 @@ package com.yellowstone.boardback.dto.response.board;
 import com.yellowstone.boardback.common.ResponseCode;
 import com.yellowstone.boardback.common.ResponseMessage;
 import com.yellowstone.boardback.dto.response.ResponseDto;
-import com.yellowstone.boardback.repository.resultSet.GetPopularResultSet;
+import com.yellowstone.boardback.repository.resultSet.GetPopularListResultSet;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +16,17 @@ public class GetPopularListResponseDto extends ResponseDto {
 
     private List<String> popularWordList;
 
-    private GetPopularListResponseDto(List<GetPopularResultSet> resultSets) {
+    private GetPopularListResponseDto(List<GetPopularListResultSet> resultSets) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         List<String> popularWordList = new ArrayList<>();
-        for(GetPopularResultSet resultSet : resultSets){
+        for(GetPopularListResultSet resultSet : resultSets){
             String popularWord = resultSet.getSearchWord();
             popularWordList.add(popularWord);
         }
         this.popularWordList = popularWordList;
     }
 
-    public static ResponseEntity<GetPopularListResponseDto> success(List<GetPopularResultSet> resultSets) {
+    public static ResponseEntity<GetPopularListResponseDto> success(List<GetPopularListResultSet> resultSets) {
         GetPopularListResponseDto result = new GetPopularListResponseDto(resultSets);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
